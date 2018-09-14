9f4f2d4d045dfa8ba4e67ae551953983d3218da5
Marcel <dragon.marcel@o2.pl>
2018-09-14 20:48:27 +0200

    initial commit

---
+++ src/main/java/warehouse/MagazynApplication.java
@@ -0,0 +1,12 @@
+package warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazynApplication.class, args);
+	}
+}
