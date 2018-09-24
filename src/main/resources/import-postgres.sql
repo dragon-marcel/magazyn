7ea14c5f86394263f5656648cc94510d60e69d10
dragon-marcel <40792273+dragon-marcel@users.noreply.github.com>
2018-09-25 00:53:26 +0200

    Update import-postgres.sql

--- src/main/resources/import-postgres.sql
+++ src/main/resources/import-postgres.sql
@@ -1,6 +1,6 @@
  
 INSERT INTO users (id,enabled,name,password,role) VALUES
-(1,true ,'admin','$2a$10$2B3t7yRJo5xUIsZGR6x1V.cSoonLYm1tKPhrGKTUNA1GEOQX3V1I','ADMIN');
+(1,true ,'admin','$2a$10$JBqoIgquukYf9ZOqrTnlU.qjSIcb1MYqDY/p9FAX7T1XIEq7mAgqq','ADMIN');
 
 INSERT INTO warehouse (id, name) VALUES (1,'Magazyn główny');
 INSERT INTO warehouse (id, name) VALUES (2,'Magazyn sklepu');
@@ -10,7 +10,15 @@ INSERT  INTO document (id, name) VALUES (2,'Przyjęcie magazynowe');
 INSERT  INTO document (id, name) VALUES (3,'Przyjęcie towaru');
 INSERT  INTO document (id, name) VALUES (4,'Klient');
 
-INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (0,1,1);
-INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (0,1,2);
+INSERT INTO products(id, name, price) VALUES (1,'telefon',200);
+INSERT INTO products(id, name, price) VALUES (2,'tablet',1000);
+INSERT INTO products(id, name, price) VALUES (3,'laptop',3099);
 
-SELECT * FROM state_products;users
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (20,1,1);
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (20,1,2);
+
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (5,2,1);
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (10,2,2);
+
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (11,3,1);
+INSERT  INTO state_products(quantity, products_id, warehouse_id) VALUES (80,3,2);
