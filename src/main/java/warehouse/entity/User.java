b90980d1822848a733816c36210d94e2726a5f85
dragon-marcel <40792273+dragon-marcel@users.noreply.github.com>
2018-09-25 10:57:45 +0200

    Update User.java

--- src/main/java/warehouse/entity/User.java
+++ src/main/java/warehouse/entity/User.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "name")
     private String name;
